fc142620ccdf9ad5c9fc9446847ae0278abf7a44
Adrian Parisi <adrian.parisi@gmail.com>
2017-01-19 23:17:28 -0300

    Add test project.

---
+++ MyLibrary.Tests/Class1Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyLibrary.Tests
+{
+    [TestClass]
+    public class Class1Test
+    {
+        [TestMethod]
+        public void GetFalseIsFalseTest()
+        {
+            Class1 class1 = new Class1();
+
+            bool expected = class1.GetFalse();
+            bool actual = false;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
